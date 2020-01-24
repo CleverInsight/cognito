dfdfa6b79005e4df3236b686095aabbca6bdc974
Bastin Robins J <bastinrobins@gmail.com>
2020-01-24 19:20:41 +0530

    Added: is_categorical method logics

--- cognito/modules/__init__.py
+++ cognito/modules/__init__.py
@@ -1,6 +1,7 @@
 """
 Data checking module
 """
+from __future__ import print_function
 import os
 import sys
 import pandas as pd
@@ -8,92 +9,100 @@ import numpy as np
 
 
 class Check(object):
-
-	def __ini__(self):
-		pass
-
-
-	@staticmethod
-	def is_categorical(column):
-		"""
-		Determines whether the specified col is categorical.
-		
-		:param      col:  column name
-		:type       col:  { pandas.series | list | tuple }
-		:return 	boolean: True | False
-
-		Usage:
-		======
-			>> Check.is_categorical(data['Age'])
-			>> True
-		"""
-		pass
-		
-
-	@staticmethod
-	def is_continuous(column):
-		"""
-		Determines whether the specified col is continuous.
-		
-		:param      col:  column name
-		:type       col:  { pandas.series | list | tuple }
-		:return 	boolean: True | False
-
-		Usage:
-		======
-			>> Check.is_continuous(data['Age'])
-			>> False
-		"""
-		pass
+    """
+    Check class helps us to identify the types of
+    variables categorical | Continuous | Discrete
+    """
+    def __ini__(self):
+        pass
 
 
-	@staticmethod
-	def is_discrete(column):
-		"""
-		Determines whether the specified column is discrete.
-		
-		:param      column:  column name
-		:type       column:  { pandas.series | list | tuple }
-		:return 	boolean: True | False
+    @staticmethod
+    def is_categorical(column):
+        """
+        Determines whether the specified col is categorical.
 
-		Usage:
-		======
-			>> Check.is_discrete(data['Age'])
-			>> True
-		"""
-		pass
+        :param      col:  column name
+        :type       col:  { pandas.series | list | tuple }
+        :return     boolean: True | False
 
+        Usage:
+        ======
+            >> Check.is_categorical(data['Age'])
+            >> True
+        """
 
-	@staticmethod
-	def is_identifier(column):
-		"""
-		Determines whether the specified column is identifier.
-		
-		:param      column:  The column
-		:type       column:  { pandas.series | list | tuple }
-		:return 	boolean: True | False
+        try:
+            return True if column.dtypes == 'object' else False
 
-		Usage:
-		======
-			>> Check.is_identifier(data['Age'])
-			>> True
+        except AttributeError:
 
-		"""
-		pass
+            print("Method only supported pandas.cores.series")
 
 
-	@staticmethod
-	def is_missing(column):
-		"""
-		Determines whether the specified column is having missing.
-		
-		:param      column:  The column
-		:type       column:  { pandas.series | list | tuple }
-		:return 	boolean: True | False
-		
-		Usage:
-		======
-			>> Check.is_missing(data['Price'])
-			>> False
-		"""
+    @staticmethod
+    def is_continuous(column):
+        """
+        Determines whether the specified col is continuous.
 
+        :param      col:  column name
+        :type       col:  { pandas.series | list | tuple }
+        :return     boolean: True | False
+
+        Usage:
+        ======
+            >> Check.is_continuous(data['Age'])
+            >> False
+        """
+        pass
+
+
+    @staticmethod
+    def is_discrete(column):
+        """
+        Determines whether the specified column is discrete.
+
+        :param      column:  column name
+        :type       column:  { pandas.series | list | tuple }
+        :return     boolean: True | False
+
+        Usage:
+        ======
+            >> Check.is_discrete(data['Age'])
+            >> True
+        """
+        pass
+
+
+    @staticmethod
+    def is_identifier(column):
+        """
+        Determines whether the specified column is identifier.
+
+        :param      column:  The column
+        :type       column:  { pandas.series | list | tuple }
+        :return     boolean: True | False
+
+        Usage:
+        ======
+            >> Check.is_identifier(data['Age'])
+            >> True
+
+        """
+        pass
+
+
+    @staticmethod
+    def is_missing(column):
+        """
+        Determines whether the specified column is having missing.
+
+        :param      column:  The column
+        :type       column:  { pandas.series | list | tuple }
+        :return     boolean: True | False
+
+        Usage:
+        ======
+            >> Check.is_missing(data['Price'])
+            >> False
+        """
