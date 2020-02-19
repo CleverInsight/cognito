12839f3279c4a9c3d3f6ea27b6659735c93be128
nibir-paul <nibirpaul491@gmail.com>
2020-02-19 12:01:40 +0530

    Changed test case for percentage missing

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -123,9 +123,9 @@ def test_encoding_categorical_3():
     assert check.encoding_categorical(df['Mjob']) == ([0, 0, 0, 1, 2, 3, 2, 2], {0: 'at_home', 1: 'at_home', 2: 'at_home', 3: 'health'})
 
 def test_percentage_missing():
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'msleep_ggplot.csv'))
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'vgsales.csv'))
     check = Check()
-    assert check.percentage_missing(df) == {'name': 0.0, 'genus': 0.0, 'vore': 8.43, 'order': 0.0, 'conservation': 34.94, 'sleep_total': 0.0, 'sleep_rem': 26.51, 'sleep_cycle': 61.45, 'awake': 0.0, 'brainwt': 32.53, 'bodywt': 0.0}
+    assert check.percentage_missing(df) == {'Rank': 0.0, 'Name': 0.0, 'Platform': 0.0, 'Year': 1.63, 'Genre': 0.0, 'Publisher': 0.35, 'NA_Sales': 0.0, 'EU_Sales': 0.0, 'JP_Sales': 0.0, 'Other_Sales': 0.0, 'Global_Sales': 0.0}
 
 def test_is_continuous_2():
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'Freedman.csv'))
