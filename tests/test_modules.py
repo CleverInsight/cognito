8a832105685fc8aad0218db751c12f4fda7e96a6
nibir-paul <nibirpaul491@gmail.com>
2020-02-20 19:19:22 +0530

    Checking

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -131,7 +131,12 @@ def test_ignore_identifier_3():
     df=pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'msleep_ggplot.csv'))
     check=Check()
     print(check.ignore_identifier(df))
-    
+
+def test_percentage_missing():
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'vgsales.csv'))
+    check = Check()
+    assert check.percentage_missing(df) == {'Rank': 0.0, 'Name': 0.0, 'Platform': 0.0, 'Year': 1.63, 'Genre': 0.0, 'Publisher': 0.35, 'NA_Sales': 0.0, 'EU_Sales': 0.0, 'JP_Sales': 0.0, 'Other_Sales': 0.0, 'Global_Sales': 0.0} 
+
 def test_is_missing_1():
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'data', 'Freedman.csv'))
     check = Check()
