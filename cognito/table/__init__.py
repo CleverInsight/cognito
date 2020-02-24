1665ae518dfbe7192f1ce2290fe281d637254e11
Bastin Robins J <bastinrobins@gmail.com>
2020-02-24 13:35:25 +0530

    Fix: Conflict in table module

--- cognito/table/__init__.py
+++ cognito/table/__init__.py
@@ -22,111 +22,7 @@ class Table:
 		"""
 		return self.data
 
-	def columns(self):
-		"""
-		Get all the column names from the given
-		dataframe `self.data`
 
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.columns()
-		"""
-		return self.data.columns
-
-
-	def total_columns(self):
-		"""
-		Get the count of all column in the given
-		dataframe `self.data`
-
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.total_columns()
-		"""
-		return len(self.columns())
-
-
-	def total_rows(self):
-		"""
-		Get total count of rows from the current 
-		dataframe `self.data`.
-
-		returns: dataframe
-
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.total_rows()
-		"""
-		pass
-
-	def get_categorical(self):
-		"""
-		Gets the categorical columns from the given
-		dataframe `self.data`
-
-		returns: dataframe
-
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.get_categorical()
-		"""
-		pass
-
-	def get_numerical(self):
-		"""
-		Gets the numerical columns from the given
-		dataframe `self.data`
-
-		returns: dataframe
-
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.get_numerical()
-		"""
-		pass
-
-
-	def odd_rows(self):
-		"""
-		Get all odd indexed counted rows from the given 
-		dataframe `self.data`
-		returns: dataframe
-
-		Usage:
-		======
-			>>> data = Table('filename.csv')
-			>>> data.odd_rows()
-		
-		"""
-		pass
-
-	def even_rows(self):
-		"""
-		Get all even indexed counted rows from the given
-		dataframe `self.data`
-
-		returns: dataframe
-		"""
-		pass
-
-
-	def summary(self):
-		"""
-		Generate descriptive summary of the given dataframe
-		`self.data`
-
-		:returns: dataframe
-		"""
-		return self.data.describe
-
-	def apply(self):
-		pass
-=======
     def columns(self):
         """
         Get all the column names from the given
@@ -139,7 +35,6 @@ class Table:
         """
 
         return self.data.columns
-
 
 
     def total_columns(self):
@@ -249,4 +144,3 @@ class Table:
             >>> df.summary()
         """
         return self.data.describe()
-        
