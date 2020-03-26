150e944521971a8772a7fea3098132945f8da5fa
Bastin Robins J <bastinrobins@gmail.com>
2020-03-26 11:01:09 +0530

    Added: Table class with empty method skeleton

--- cognito/table/__init__.py
+++ cognito/table/__init__.py
@@ -120,6 +120,8 @@ class Table:
 
     def even_rows(self):
         """
+        # TODO:
+
         Get all even indexed counted rows from the given
         dataframe `self.data`
 
@@ -176,6 +178,8 @@ class Table:
 
     def correlation(self, mode="pearson"):
         """
+        # TODO:
+
         Return the pairwise correlation of the given 
         dataframe `self.data` and return dataframe with
         respective dataframe.
@@ -199,6 +203,8 @@ class Table:
 
     def covariance(self):
         """
+        # TODO:
+
         Return the covariance of the given 
         dataframe `self.data` and return dataframe with
         respective dataframe.
@@ -214,3 +220,207 @@ class Table:
         """
 
         pass
+
+
+
+    def slice(self, columns):
+        """
+        # TODO:
+
+        Return only the columns sliced 
+        from `self.data` based on given `columns` parameter
+
+        
+        :param      columns:  list or tuple
+        :type       columns:  { list of column name  }
+
+        returns: dataframe of only given column names.
+
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.slice(['name', 'age'])
+            ---------------
+              name  | age
+            ---------------
+              Sam   | 20
+            ---------------
+              Jack  | 40
+            ---------------
+
+        """
+        # TODO:
+
+        pass
+
+
+
+    def binning(self, col, bins, labels):
+        """
+        Return dataframe of select column convert into bins as given
+        parameter
+        
+        :param      col:   The col
+        :type       col:   { column name to be selected }
+        :param      bins:  The bins
+        :type       bins:  list of  of bins to convert
+        :param      bins:  The bins
+        :type       bins:  list of  of bins to convert
+
+    
+        returns: dataframe of given column as bins
+
+        example:
+            # Numerical Binning Example
+                Value      Bin       
+                0-30   ->  Low       
+                31-70  ->  Mid       
+                71-100 ->  High
+
+            # Categorical Binning Example
+                Value      Bin       
+                Spain  ->  Europe      
+                Italy  ->  Europe       
+                Chile  ->  South America
+                Brazil ->  South America
+
+
+        Usage:
+        ======
+
+
+
+            >>> df = Table('filename.csv')
+            >>> df.binning('age', bins=[], labels=['low', 'med', 'high'])
+
+            ------------------
+            |  age  | age_bin 
+            ------------------
+            |  14   | low
+            ------------------
+            |  90   | high
+            ------------------
+            |  14   | low
+            ------------------
+            |  40   | middle
+            ------------------
+            |  14   | low
+            ------------------
+    
+
+        """
+        # TODO:
+
+        pass
+
+
+    def fix_outlier(self, column):
+        """
+        Take the column from `self.data` and check for outlier 
+        fix the outlier using std deviation, percentile, drop and cap
+
+        Ref: https://towardsdatascience.com/feature-engineering-for-machine-learning-3a5e293a5114
+        
+        :param      column:  The column
+        :type       column:  { column name as string }
+        
+        returns: dataframe without outlier
+
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.fix_outlier('age')
+
+        """
+
+        # TODO:
+
+        pass
+
+
+
+    def fix_missing(self, column):
+        """
+        Take the column from `self.data` and fix the type of columns
+        and clean the missing values based on categorical or numerical
+        
+        :param      column:  The column
+        :type       column:  { column name }
+
+        returns: dataframe | pandas.core.series
+
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.fix_missing('age')
+
+        """
+
+        # TODO:
+
+        pass
+
+
+    def imputer(self, column):
+        """
+        Take the columns from `self.data` and fill the missing NA
+        values based on the type of column
+        
+        :param      column:  The column
+        :type       column:  { column name }
+        
+        returns: dataframe | pandas.core.series
+
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.imputer('age')
+        """
+
+        # TODO:
+
+        pass
+
+
+    def ignore_cardinal(self):
+        """
+        Take the dataframe `self.data` and remove all cardinality columns
+        and return the dataframe
+        
+        returns: dataframe without cardinality columns
+
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.ignore_cardinal()
+
+        """
+
+        # TODO:
+
+        pass
+
+
+    def encode_column(self, column):
+        """
+        Encodes a column using the numerical values and 
+        return the dictionary for mapping.
+        
+        :param      column:  The column
+        :type       column:  { column name }
+
+        returns: dataframe, mapper dictionary
+        
+        Usage:
+        ======
+            >>> df = Table('filename.csv')
+            >>> df.encode_column('Country')
+            >>> (dataframe, {'0': 'US', '1': 'India', '2': 'Europe'})
+
+        """
+
+        # TODO:
+        
+        pass
+
+
