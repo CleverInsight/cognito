1cbce0bc3d1cae2978b1f06f6985f0c65b6bee1d
Bastin Robins J <bastinrobins@gmail.com>
2020-03-28 22:35:38 +0530

    Fix: pylint for setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
+"""
+Cognito module
+"""
 from codecs import open as codecs_open
 from setuptools import setup, find_packages
 
 
 # Get the long description from the relevant file
 with codecs_open('README.rst', encoding='utf-8') as f:
-    long_description = f.read()
+    LONG_DESCRIPTION = f.read()
 
 
-install_requires = [
-  'numpy',  # REQ: vector algebra operations
-  'scipy',
-  'numpy',
-  'click',            # REQ: command line interfacing
-  'pandas',           # REQ: (conda) sparx.data.filter()
-  'PyYAML',
-  'pyfiglet',
-  'datefinder',
-  'PrettyTable'
+REQUIRES = [
+    'numpy',  # REQ: vector algebra operations
+    'scipy',
+    'numpy',
+    'click',            # REQ: command line interfacing
+    'pandas',           # REQ: (conda) sparx.data.filter()
+    'PyYAML',
+    'pyfiglet',
+    'datefinder',
+    'PrettyTable'
 ]
 
 
@@ -42,7 +45,7 @@ CLASSIFIERS = [
 setup(name='cognito',
       version='0.0.1',
       description=u"Auto ML Dataset Transformer",
-      long_description=long_description,
+      long_description=LONG_DESCRIPTION,
       classifiers=CLASSIFIERS,
       keywords='',
       author=u"Bastin Robins .J",
@@ -52,14 +55,13 @@ setup(name='cognito',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=install_requires,
-      
+      install_requires=REQUIRES,
+
       extras_require={
           'test': ['pytest'],
       },
-      
+
       entry_points="""
       [console_scripts]
       cognito=cognito.scripts.cli:cli
-      """
-)
+      """)
