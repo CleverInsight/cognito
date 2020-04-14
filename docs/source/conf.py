2c02f2400612f2fcbfc07f194fba9335e12dc4bb
Bastin Robins J <bastinrobins@gmail.com>
2020-04-14 16:11:12 +0530

    Added: Collaborator into the project

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ import sphinx_rtd_theme
 
 extensions = [
     "sphinx_rtd_theme",
+    "sphinx_gallery.gen_gallery"
 ]
 
 # -- General configuration ------------------------------------------------
