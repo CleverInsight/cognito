c16b0b75bf1ca5db69a48b51c5d7ecd6db9d96a5
Bastin Robin <bastinrobins@gmail.com>
2020-06-17 10:13:51 +0530

    Update test_command.py
    Removed empty imports

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,8 +1,1 @@
-from cognito.check import Check
-from cognito.table import Table
-import os
-import pytest
-import pandas as pd
-import numpy as np
-from os import path 
-from sklearn import preprocessing
+
